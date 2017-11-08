c292aca68f51d5ba6e20cc1b807a9bb3a938f3b3
MA <MA>
2017-11-08 23:18:31 +0100

    complete_project

---
+++ machine-learning-ex1/ex1/plotData.m
@@ -0,0 +1,31 @@
+
+function plotData(x, y)
+
+figure('Name','Datos de entrada'); % open a new figure window
+plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data
+ylabel('Beneficio en 10K'); % Set the y?axis label
+xlabel('Poblacion de la ciudad en 10K '); % Set the x?axis label
+
+%PLOTDATA Plots the data points x and y into a new figure 
+%   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
+%   population and profit.
+
+end
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the training data into a figure using the 
+%               "figure" and "plot" commands. Set the axes labels using
+%               the "xlabel" and "ylabel" commands. Assume the 
+%               population and revenue data have been passed in
+%               as the x and y arguments of this function.
+%
+% Hint: You can use the 'rx' option with plot to have the markers
+%       appear as red crosses. Furthermore, you can make the
+%       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
+
+
+
+
+
+% ============================================================
+
+
